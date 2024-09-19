6a84e83d773aeb9f28bced55792547b9e51ed079
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-09-19 23:01:41 +0200

    fix: remove unnecessary inputs

--- flake.nix
+++ flake.nix
@@ -1,14 +1,11 @@
 {
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
       flake.flakeModule = ./flake-module.nix;
     };
 }
